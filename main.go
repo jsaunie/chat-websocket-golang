375fcecf008914122a1337ff00b14106160e8a56
Jean Saunie <jean.saunie@devinci.fr>
2018-12-01 22:33:31 +0100

    Chat

--- main.go
+++ main.go
@@ -4,39 +4,37 @@ import (
 	"fmt"
 	"golang.org/x/net/websocket"
 	"net/http"
-	"reflect"
 )
 
 func main() {
-	http.Handle("/ws/", websocket.Handler(connHandler))
+	hub := make([]*websocket.Conn, 0)
+
+	http.Handle("/chat/", websocket.Handler(func(ws *websocket.Conn) {
+		data := map[string]interface{}{}
+		hub = append(hub, ws)
+
+		for {
+			err := websocket.JSON.Receive(ws, &data)
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+			fmt.Println(data)
+			for i := range hub {
+				err2 := websocket.JSON.Send(hub[i], data)
+				if err2 != nil {
+					fmt.Println(err2)
+					break
+				}
+			}
+		}
+		err3 := ws.Close()
+		if err3 != nil {
+			fmt.Println(err3)
+		}
+	}))
 	err := http.ListenAndServe(":5555", nil)
 	if err != nil {
 		fmt.Println(err)
-	}
-}
-
-func connHandler(ws *websocket.Conn) {
-	data := map[string]interface{}{}
-
-	for {
-		err := websocket.JSON.Receive(ws, &data)
-		if err != nil {
-			fmt.Println(err)
-			break
-		}
-		fmt.Printf("Server receive : %v\n", data)
-		fmt.Printf(
-			"Types received : %s\n",
-			reflect.TypeOf(data["text"]),
-		)
-		err2 := websocket.JSON.Send(ws, data)
-		if err2 != nil {
-			fmt.Println(err2)
-			break
-		}
-	}
-	err3 := ws.Close()
-	if err3 != nil {
-		fmt.Println(err3)
 	}
 }
